0d227aff9a2569da1a646124c475c09051e50d3c
chinmaya123 <10561916+chinmaya123@users.noreply.github.com>
2020-09-22 16:25:29 +0530

    Chnages to compute the inverse of the matrix if it doesn't find the inverse in cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to calculate the inverse of matrix if it doesn't find the inverse 
+## in the cache
 
-## Write a short comment describing this function
+## makeCacheMatrix is used to create a special object that stores 
+##a matrix and cache's its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve compute the inverse if it doesn't find the inverse in the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
